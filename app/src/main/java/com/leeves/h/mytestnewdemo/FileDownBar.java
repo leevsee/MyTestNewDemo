1d4008f3604287c6c3e31fbb1aa5e965e5ce59dc
leevsee <237167099@qq.com>
2016-10-01 15:56:39 +0800

    FiledDownBar

---
+++ app/src/main/java/com/leeves/h/mytestnewdemo/FileDownBar.java
@@ -0,0 +1,17 @@
+package com.leeves.h.mytestnewdemo;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Function：
+ * Created by h on 2016/9/30.
+ *
+ * @author Leeves
+ */
+
+@EActivity
+public class FileDownBar extends Activity {
+
+}
